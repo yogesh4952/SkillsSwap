fe1631a4a2451354cf5df633ad51e045200f9671
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-20 19:24:45 +0545

    Check some function work

--- server/prac.js
+++ server/prac.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect('mongodb://localhost:27017/mvc_todo', {
+  .connect('', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
