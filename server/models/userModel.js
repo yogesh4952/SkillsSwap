98aab7838e6175a77268c6048e263c5af7860b44
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-28 13:36:08 +0545

    Remove node modules file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+const activitySchema = new mongoose.Schema({
+  firstname: {
+    type: string,
+    required: true,
+  },
+  lastname: {
+    type: string,
+    required: true,
+  },
+});
+
+export default mongoose.model('Activity', activitySchema);
